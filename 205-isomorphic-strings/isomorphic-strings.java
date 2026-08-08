dba40d2f6a8f7d79de6c4c2d976e4928a9263a4b
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-08 22:57:03 +0530

    Time: 5 ms (98.57%) | Memory: 43.8 MB (59.28%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int slen=s.length();
+        int tlen=t.length();
+
+        //take one string(s). then traverse it, take its char of index i. convert it to ascii. then go to the char array's index=ascii. If that index's value is null(\u0000) then: store the character of other string's index i (t.charAt(i)). else if that index is already filled with other char which NOT t.charAt(i) then it's one-to-many mapping so, return false.
+        //do same taking string(t) to traverse on it. its needed to find many-to-one maps as well which arnt detected by first loop.
+
+        char[] arr =  new char[128];
+        for(int i=0; i<slen; i++){
+            int ascii=(int)s.charAt(i);
+            if(arr[ascii]=='\u0000') arr[ascii]=t.charAt(i);
+            else if(arr[ascii]!=t.charAt(i)) return false;
+        }
+
+        char[] arr2 = new char[128];
+        for(int i=0; i<tlen; i++){
+            int ascii=(int)t.charAt(i);
+            if(arr2[ascii]=='\u0000') arr2[ascii]=s.charAt(i);
+            else if(arr2[ascii]!=s.charAt(i)) return false;
+        }
+        return true;
+    }
+}
