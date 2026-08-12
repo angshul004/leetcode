6b64823a51bf63fec7fdbefc679a701b1d2151ce
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-12 17:13:04 +0530

    Time: 75 ms (9.56%) | Memory: 55.9 MB (18.27%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+
+        //using insertion sort
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j>0; j--){
+                if(nums[j]<nums[j-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j-1];
+                    nums[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        return nums[n/2];
+    }
+}
