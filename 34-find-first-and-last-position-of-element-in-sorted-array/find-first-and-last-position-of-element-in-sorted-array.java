35487f8d1bc855039613d7942ad91e19ff5bef37
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-13 17:47:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (14.25%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,6 +3,7 @@ class Solution {
         int[] soln= {-1,-1};
 
         //finding lower bound - it will find the first occurance
+        //lb means the element which is equal or just bigger than target
         int lo=0, hi=nums.length-1, mid;
         int lb= nums.length;
         while(lo<=hi){
@@ -15,6 +16,7 @@ class Solution {
             }
         }
         //finding upper bound - it will find the next element after last occurance
+        //ub means the element which is just bigger than target
         lo=0; hi=nums.length-1;
         int ub= nums.length;
         while(lo<=hi){
@@ -27,6 +29,7 @@ class Solution {
             }
         }
 
+        //this check is importent before setting soln values. cuz lb points to index=length when target isnt present & all elements are less than target. And also, when target isnt present it points to number just bigger than the target. in those cases we must not update soln values and keep -1,-1. this condition also makes sure isnt wrong. cuz if lb is valid and points to target, ub is guaranteed to be correct.
         if(lb<nums.length && nums[lb]==target){
             soln[0]=lb;
             soln[1]=ub-1;
