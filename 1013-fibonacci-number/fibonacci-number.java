4bd4027c11b0879bb01f11ab4518cfafbf713601
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-21 13:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,5 +1,15 @@
 class Solution {
+    static int[] arr;
+    public int fibdp(int n){
+        if(n<=1) return n;  //base case
+        if(arr[n]!=0) return arr[n];
+
+        int ans= fibdp(n-1)+fibdp(n-2);
+        arr[n]=ans;
+        return ans;
+    }
     public int fib(int n) {
+        /* normal way:
         if(n<=1) return n;
         int n0=0, n1=1;
         int sum=0;
@@ -9,5 +19,10 @@ class Solution {
             n1=sum;
         }
         return sum;
+        */
+
+        // dynamic programming:
+        arr = new int[n+1];    //need index values 0 to n
+        return fibdp(n);
     }
 }
