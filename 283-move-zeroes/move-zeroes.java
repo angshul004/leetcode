df6e448df9ebd7e9b453a5bdf33c2ab1d9d75d7b
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-21 18:17:29 +0530

    Time: 2 ms (91.97%) | Memory: 47.8 MB (57.71%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,8 +1,8 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        //using concept of bubble sort. instead of swapping bigger element, swap zeroes to right.
-
+        //using concept of bubble sort. instead of swapping bigger element, swap zeroes to right. 
         int n= arr.length;
+        /*
         for(int i=0; i<n-1; i++){
             boolean sorted=true;
 
@@ -16,6 +16,18 @@ class Solution {
             }
 
             if(sorted) break;
+        }
+        */
+
+        //using only 1 loop:
+        int j=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                j++;
+            }
         }
     }
 }
