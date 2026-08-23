e36ddb87988ea07e1d7a6bd80d1c577a5c6c1ffa
Angshul Sana <167230304+angshul004@users.noreply.github.com>
2026-08-23 13:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (47.53%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if(N<0) return 1.0/pow(x, -N);
+        else return pow(x, N);
+
+    }
+    public double pow(double x, long n){
+
+        if(n==0) return 1.0;
+
+
+        double ans = pow(x,n/2);        //this makes TC O(logn)
+        if(n%2==0) return ans*ans;
+        else return ans*ans*x;
+    }
+}
